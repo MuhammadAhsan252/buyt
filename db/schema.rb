08575cc49a37e13920d2f8eb9fadd51ffe4238d1
AhsanAkbar252 <ahsanakbar252@gmail.com>
2025-05-19 14:03:16 +0500

    added buyer registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_19_065244) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_19_081527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "buyers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.text "bio"
+    t.string "position"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.string "zip_code"
+    t.string "industry"
+    t.string "ai_strategy"
+    t.string "solutions", default: [], array: true
+    t.string "pain_points", default: [], array: true
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_buyers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_buyers_on_reset_password_token", unique: true
+  end
 
   create_table "sellers", force: :cascade do |t|
     t.string "email", default: "", null: false
