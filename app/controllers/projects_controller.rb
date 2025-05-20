4b8e9399a792d8c447c68f52a59ab2b90c88b74a
AhsanAkbar252 <ahsanakbar252@gmail.com>
2025-05-20 17:07:49 +0500

    lint issues recolved

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,12 +2,12 @@ class ProjectsController < ApplicationController
     before_action :set_project, only: [ :edit, :update, :destroy ]
     before_action :authenticate_seller!
     before_action :set_seller
-  
+
     # GET /projects/new
     def new
       @project = Project.new
     end
-  
+
     # POST /projects
     def create
       @project = Project.new(project_params)
@@ -18,11 +18,11 @@ class ProjectsController < ApplicationController
         render :new
       end
     end
-  
+
     # GET /projects/:id/edit
     def edit
     end
-  
+
     # PATCH/PUT /projects/:id
     def update
       if @project.update(project_params)
@@ -31,28 +31,27 @@ class ProjectsController < ApplicationController
         render :edit
       end
     end
-  
+
     # DELETE /projects/:id
     def destroy
       @project.destroy
       redirect_to seller_url(@seller), notice: "project was successfully destroyed."
     end
-  
+
     private
-  
+
     def set_project
       @project = Project.find(params[:id])
     end
-  
+
     def project_params
       params.require(:project).permit(:name, :details, :link)
     end
-  
+
     def set_seller
       @seller = Seller.find(params[:seller_id])
       unless current_seller == @seller
           redirect_to root_path, alert: "You are not authorized to manage projects for this seller."
       end
     end
-  end
-  
+end
