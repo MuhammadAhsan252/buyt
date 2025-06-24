5a44fb809ed6286da477d656ea6237abebb63e6a
AhsanAkbar252 <ahsanakbar252@gmail.com>
2025-06-24 17:12:48 +0500

    removed solid queue

--- config/initializers/solid_queue.rb
+++
@@ -1,8 +0,0 @@
-if Rails.env.production?
-    SolidQueue.database_configuration = {
-      "production" => {
-        "url" => ENV["DATABASE_URL"],
-        "adapter" => "postgresql"
-      }
-    }
-end
